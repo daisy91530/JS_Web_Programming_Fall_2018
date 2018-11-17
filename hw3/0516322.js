c2e1a5ca829511992565b7528b7db495ed634d56
daisy91530 <daisy91530@gmail.com>
2018-11-18 00:21:08 +0800

    finish hw3

--- hw3/0516322.js
+++ hw3/0516322.js
@@ -2,8 +2,11 @@ var i, j, k; // 不宣告也能用 但要好維護
 document.writeln("<h1>Daisy's Magic Game</h1>");
 document.writeln("<h3>Please find a number between 1 and 64, I will read your mind and find it immediately.</h3>");
 document.writeln('<h3 id="message">What is your number ? </h3> ');
-document.writeln('<input type = "submit" value = "Submit value" onclick = "guess()">');
-
+document.writeln("<div>");
+document.writeln('<input class="btn" type = "submit" value = "Submit" onclick = "guess()">');
+document.writeln('<input class="btn" type = "button" value = "Uncheck" onclick="reset()">');
+document.writeln("</div>");
+document.writeln("<div>");
 for (i=0;i<6;i++){
 	document.writeln('<table>');
 	document.writeln('<tr>'
@@ -15,29 +18,22 @@ for (i=0;i<6;i++){
 	for(j=0;j<32;j++){
 		if (j%8==0) document.write('<tr class="good">');
 		k = j*2+Math.pow(2, i)-j%Math.pow(2, i);
-		document.write('<td name="num',k,'" >' + k + '</td>');
+		document.write('<td id="ye" name="num',k,'" >'+k+'</td>');
 		if (j%8==7) document.write("</tr>");
 	}
 	document.writeln("</table>");
 }
-
-var flashnum=0, timer, answer;
+document.writeln("</div>");
+var flashnum=0, timer, ans;
 
 function blink(bingo){
 	var o, i;
-
+	o = document.getElementsByName("num"+flashnum);
+	for(i=0;i<o.length;i++) o[i].className="good";
 	flashnum = bingo || Math.floor(Math.random()*63) + 1;
 	o = document.getElementsByName("num"+flashnum);
-	
-	for(i=0;i<=o.length;i++){
-		if(o[i].innerText==flashnum){
-			o[i].style.backgroundColor="#FF6EFF";
-		}else{
-			o[i].style.backgroundColor="#FF6E00";
-			//document.writeln("g");
-		}
-	}
-	var o = document.getElementById("message");
+	for(i=0;i<o.length;i++) o[i].className="goodd";
+	o = document.getElementById("message");
 	o.innerText = o.innerText + "." ;
 }
 
@@ -64,4 +60,9 @@ function guess(){
 	start();
 }
 
-
+function reset(){
+	var inputs = document.getElementsByName("card");
+  	for(i = 0; i < inputs.length; i++) {
+    	inputs[i].checked = false;
+  	}
+}
--- hw3/README.md
+++ hw3/README.md
@@ -2,4 +2,7 @@
 
 Display six cards that can play the binary cards game 
 Finish the magic card game
-Details are int the pdf
+Details are in the pdf
+
+### 答題過程
+首先勾選出卡片，這時候還來得及反悔，按下uncheck按鈕可以清除我以勾勾，選好後，加上summit按鈕，按下之後呼叫 guess()這個function先算出嗄案，算出後再呼叫start() 設下timer 每500ms call 一次blink()也就是閃一次，每次閃都先隨機找一個數字，每個卡片上的那個數字背景變色，同時用「.」來當作電腦正在計算。總共閃六次，所以設3000ms才呼叫stop()這個函數，這時候顯示答案是多少，然後讓答案背景變色。
